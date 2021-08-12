8bb4d0335ae89cf59c6350832203970cf5975981
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2021-08-12 19:26:41 +0530

    Create main.js

---
+++ Project 1/assets/js/main.js
@@ -0,0 +1,160 @@
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+          selectEl.addEventListener(type, listener)
+        }
+      }
+    }
+  
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+    
+  
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200
+      navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          navbarlink.classList.add('active')
+        } else {
+          navbarlink.classList.remove('active')
+        }
+      })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+  
+  
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+      let elementPos = select(el).offsetTop
+      window.scrollTo({
+        top: elementPos,
+        behavior: 'smooth'
+      })
+    }
+  
+    /**
+     * Scroll with offset on links with a class name .scrollto
+     */
+    on('click', '.scrollto', function(e) {
+      if (select(this.hash)) {
+        e.preventDefault()
+  
+        let navbar = select('#navbar')
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
+      }
+    }, true)
+  
+  
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: "ease-in-out",
+        once: true,
+        mirror: false
+      });
+    });
+  
+  })()
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+  
+  
+  var imageDivs = document
+    .querySelector(".caroussel-container")
+    .querySelectorAll(".image img");
+  imageDivs.forEach((imageDiv) => {
+    imageDiv.width = window.innerWidth;
+  });
+  
+  let moved = false;
+  const windowWidth = window.innerWidth;
+  function slide() {
+    if (!moved) {
+      ele = document.querySelector(".caroussel");
+      left = ele.style.left;
+      left = left.replace("px", "");
+      newLeft = Number(left) - windowWidth;
+      if (newLeft == -6 * windowWidth) {
+        newLeft = 0;
+      }
+      // console.log(newLeft);
+      ele.style.left = `${newLeft}px`;
+      // colorRoundButton(newLeft.toFixed(2));
+    } else {
+      moved = false;
+    }
+  }
+  window.onload = (e) => {
+    setInterval(slide, 2000);
+  };
